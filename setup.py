b51db329a73fd09c7d907ef9c0950cbb62689cc5
Bevilacqua Jean-Sebastien <realitix@gmail.com>
2018-06-14 18:55:38 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.command.build import build
+from setuptools import setup
+from os import path
+import platform
+
+
+setup(
+    name="pywaylandegl",
+    version="1.0",
+    author="realitix",
+    author_email="realitix@gmail.com",
+    description="Python CFFI binding for Wayland-EGL",
+    long_description="Python CFFI binding for Wayland-EGL",
+    packages=['pywaylandegl'],
+    install_requires=["cffi"],
+    setup_requires=["cffi"],
+    include_package_data=True,
+    url="http://github.com/realitix/pywaylandegl",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    license="Apache 2.0",
+    ext_package="pywaylandegl",
+    cffi_modules=["pywaylandegl/pywaylandegl_build/pywaylandegl_build.py:ffibuilder"]
+)
