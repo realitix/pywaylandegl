e685fb2e5ce5d5801b2646abfaaa1cd8abf1ad5c
Bevilacqua Jean-Sebastien <realitix@gmail.com>
2018-06-14 19:32:52 +0200

    Add documentation

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# pywaylandegl
-Python module allowing the call to wayland-egl library
+# Python Wayland-egl binding
+
+This module provides access to the `wayland-egl` library from Python.
+
+To use EGL with Wayland, you have to call this library, so here is the wrapper.
+This library exposes the following function:
+
+```python
+def wl_egl_window_create(wl_surface, width, height):
+    """Create the wayland-egl window
+
+    Args: 
+        wl_surface (ptr): The wayland surface. You can get it with the
+                          pywayland library.
+        width (int): Window width
+        height (int): Window height
+    """
+
+def wl_egl_window_resize(wl_egl_window, width, height, dx, dy):
+    """Resize the wayland-egl window
+
+    Args:
+        wl_egl_window (window): The window created with `wl_egl_window_create`
+        width (int): The new window width
+        height (int): The new window height
+        dx (int): Don't know
+        dy (int): Idem
+    """
+
+def wl_egl_window_destroy(wl_egl_window):
+    """Destroy the window-egl window
+
+    Args:
+        wl_egl_window (window): The window created with `wl_egl_window_create`
+    """
+
+def wl_egl_window_get_attached_size(wl_egl_window):
+    """Destroy the window-egl window
+
+    Args:
+        wl_egl_window (window): The window created with `wl_egl_window_create`
+
+    Returns:
+        (int, int): Width and height
+    """
+```
--- pywaylandegl/__init__.py
+++ pywaylandegl/__init__.py
@@ -2,18 +2,48 @@ from pywaylandegl._pywaylandegl import ffi, lib
 
 
 def wl_egl_window_create(wl_surface, width, height):
+    """Create the wayland-egl window
+
+    Args: 
+        wl_surface (ptr): The wayland surface. You can get it with the
+                          pywayland library.
+        width (int): Window width
+        height (int): Window height
+    """
     return lib.wl_egl_window_create(wl_surface, width, height)
 
 
 def wl_egl_window_resize(wl_egl_window, width, height, dx, dy):
+    """Resize the wayland-egl window
+
+    Args:
+        wl_egl_window (window): The window created with `wl_egl_window_create`
+        width (int): The new window width
+        height (int): The new window height
+        dx (int): Don't know
+        dy (int): Idem
+    """
     lib.wl_egl_window_resize(wl_egl_window, width, height, dx, dy)
 
 
 def wl_egl_window_destroy(wl_egl_window):
+    """Destroy the window-egl window
+
+    Args:
+        wl_egl_window (window): The window created with `wl_egl_window_create`
+    """
     lib.wl_egl_window_destroy(wl_egl_window)
 
 
 def wl_egl_window_get_attached_size(wl_egl_window):
+    """Destroy the window-egl window
+
+    Args:
+        wl_egl_window (window): The window created with `wl_egl_window_create`
+
+    Returns:
+        (int, int): Width and height
+    """
     width = ffi.new('int*')
     height = ffi.new('int*')
     lib.wl_egl_window_get_attached_size(wl_egl_window, width, height)
